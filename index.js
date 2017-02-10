4d6ede9ce3b8e254f971ff7d8beb4c04e06d8b7d
Juan van den Anker <juan@vdanker.net>
2017-02-10 23:01:42 +1300

    added menubar

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const electron = require('electron');
 const virtualbox = require('virtualbox');
 const {ipcMain} = require('electron');
-
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
+const menubar = require('menubar')
 const path = require('path');
 const url = require('url');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const mb = menubar({
+    index: url.format({
+        pathname: path.join(__dirname, 'public/test.html'),
+        protocol: 'file:',
+        slashes: true
+    }),
+    preloadWindow: true});
+
 let mainWindow;
 
 function createWindow () {
@@ -32,8 +37,11 @@ function createWindow () {
   mainWindow.on('closed', function () {
     mainWindow = null
   });
-
 }
+
+mb.on('show', function() {
+    console.log('show');
+});
 
 ipcMain.on('/api/machines', (event, arg) => {
   console.log(arg);
@@ -55,27 +63,20 @@ ipcMain.on('synchronous-message', (event, arg) => {
   event.returnValue = 'pong2';
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+mb.on('ready', function ready () {
+  console.log('app is ready');
+});
+
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
