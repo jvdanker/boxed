67b6506d1d6a7c0335748c26a29c0af51c4e5daa
Juan van den Anker <juan.anker@dia.govt.nz>
2017-02-13 15:44:44 +1300

    removed test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-console.log('test');
-
-var virtualbox = require('virtualbox');
-
-virtualbox.list(function list_callback(machines, error) {
-  if (error) throw error;
-
-  console.log(machines);
-});
